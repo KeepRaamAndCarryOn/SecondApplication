0b91054a8b1533dd129adff5cc409b259691a558
KeepRaamAndCarryOn <raam235k@gmail.com>
2019-02-04 00:15:10 +0800

    Setup button layout. Perform recursive addition.

--- app/src/main/java/com/example/secondapplication/MainActivity.java
+++ app/src/main/java/com/example/secondapplication/MainActivity.java
@@ -2,12 +2,80 @@ package com.example.secondapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button numericButton;
+    Button operatorButton;
+
+    TextView eqTextView;
+    TextView rhsTextView;
+    TextView lhsTextView;
+
+
+    String eqStr;
+    String rhsStr;
+
+    Integer LHS;
+    Integer RHS;
+    Integer res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eqTextView = (TextView) findViewById(R.id.eqTextBox);
+        rhsTextView = (TextView) findViewById(R.id.rhsView);
+        lhsTextView = (TextView) findViewById(R.id.lhsView);
+
+        eqStr = "";
+        rhsStr = "";
+        LHS = null;
+        RHS = null;
+
+
+    }
+
+
+    public void numericClick(View view){
+        numericButton = (Button) view;
+
+        eqStr += numericButton.getText().toString();
+        rhsStr += numericButton.getText().toString();
+        eqTextView.setText(eqStr);
+        //rhsTextView.setText(rhsStr);
+        updateDisplay();
+    }
+
+    public void operatorClick(View view){
+        operatorButton = (Button) view;
+
+        if(LHS == null)        {
+            LHS = Integer.decode(rhsStr);
+            eqStr += "+";
+        }
+        else {
+            RHS = Integer.decode(rhsStr);
+            res = LHS + RHS;
+            LHS = res;
+
+            eqStr = String.format("%s+", String.valueOf(res));
+        }
+        rhsStr = "";
+        RHS = 0;
+        updateDisplay();
+    }
+
+    public void updateDisplay()
+    {
+        eqTextView.setText(eqStr);
+        rhsTextView.setText(rhsStr);
+        if(LHS != null) {
+            lhsTextView.setText(String.valueOf(LHS.intValue()));
+        }
     }
 }
