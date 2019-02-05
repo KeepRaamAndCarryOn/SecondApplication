249f9cdfafc00e46d246d74529badafd9b106058
KeepRaamAndCarryOn <raam235k@gmail.com>
2019-02-05 11:12:08 +0800

    Implemented Paid and Spent Column. Able to add current result to Paid column on pressing the Paid button.

---
+++ README.md
@@ -0,0 +1,12 @@
+### Objective:
+To be able to note multiple entries of "paid" and "spent" for multiple instances of "person", and resolve the dues among one another
+
+### Constraint Layout
+Use constraint layout as the views can be arranged relative to each other.
+* Use Vertical Guidelines to create a universal offset of 8dp from either edge
+* Use Horizontal Guidelines, defined by percentage, around midway of the screen. The area below the guideline will be the calculator area
+* Use chains, horizontally and vertically so that the buttons space themselves out in the area. Button width and height should match constraint so they fill up the space.
+* This creates a clean matrix of buttons, which would be scalable to different screen sizes
+
+### ArrayMap
+Define the array map with data types of the Key and Value. "int" is a primitive type, is not allowed. Use "Integer" instead.
--- app/src/main/java/com/example/secondapplication/MainActivity.java
+++ app/src/main/java/com/example/secondapplication/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.util.ArrayMap;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView eqTextView;
     TextView eqTopView;
+
+    LinearLayout paidColumn;
 
 
     String bottom_str;
@@ -26,10 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     Op op;
 
-    enum Op{
+    enum Op {
         NONE, ADD, MINUS, MULTIPLY, DIVIDE, EQUALS
     }
-
 
 
     ArrayMap<Integer, Op> op_table;
@@ -42,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         eqTextView = (TextView) findViewById(R.id.eqTextBox);
         eqTopView = (TextView) findViewById(R.id.eqTop);
+
+        paidColumn = findViewById(R.id.paidLinearLayout);
 
         op_table = new ArrayMap<>();
         op_table.put(R.id.buttonPlus, Op.ADD);
@@ -60,14 +66,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void numericClick(View view){
+    public void numericClick(View view) {
         numericButton = (Button) view;
         bottom_str += numericButton.getText().toString();
         rhs_str += numericButton.getText().toString();
         updateDisplay();
     }
 
-    public void operatorClick(View view){
+    public void operatorClick(View view) {
         operatorButton = (Button) view;
 
 
@@ -84,24 +90,19 @@ public class MainActivity extends AppCompatActivity {
         updateDisplay();
     }
 
-    public void compute()
-    {
-        if (top_str.equals("")){
-        LHS = 0.0;
+    public void compute() {
+        if (top_str.equals("")) {
+            LHS = 0.0;
         }
 
-        if(rhs_str.equals(""))        {
+        if (rhs_str.equals("")) {
             RHS = 0.0;
-        }
-        else{
+        } else {
             RHS = Double.valueOf(rhs_str);
         }
 
-
-        //LHS = LHS+RHS;
-        if (op != null)
-        {
-            switch(op){
+        if (op != null) {
+            switch (op) {
                 case ADD:
                     LHS = LHS + RHS;
                     break;
@@ -113,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void equalClick(View view){
+    public void equalClick(View view) {
 
         compute();
         top_str = String.valueOf(LHS);
@@ -125,12 +126,71 @@ public class MainActivity extends AppCompatActivity {
         updateDisplay();
     }
 
-    public void updateDisplay(){
+    View.OnClickListener entyrClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    public class Entry {
+        Double amount;
+    }
+
+    public void PaidClick(View view) {
+        if (top_str.equals(""))
+            return;
+
+        Entry newEntry = new Entry();
+        newEntry.amount = LHS;
+
+        TextView newEntryView = createEntryView(newEntry.amount.toString());
+        paidColumn.addView(newEntryView);
+        clearDisplay();
+    }
+
+    public TextView createEntryView(String s) {
+        TextView ret;
+        ret = new TextView(this);
+
+        /*TextView
+        android:id="@+id/textView2"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:clickable="true"
+        android:onClick="PaidClick"
+        android:text="TextView"
+        android:textAlignment="center"
+        tools:layout_editor_absoluteX="16dp"
+        tools:layout_editor_absoluteY="16dp"*/
+
+        ret.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,    //width
+                LinearLayout.LayoutParams.WRAP_CONTENT));  //height
+
+        ret.setClickable(true);
+        ret.setOnClickListener(entyrClickListener);
+        ret.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        ret.setText(s);
+
+        return ret;
+    }
+
+    public void clearDisplay() {
+        LHS = 0.0;
+        RHS = 0.0;
+        top_str = "";
+        bottom_str = "";
+        rhs_str = "";
+        op = Op.ADD;
+        updateDisplay();
+    }
+
+    public void updateDisplay() {
         eqTopView.setText(top_str);
         eqTextView.setText(bottom_str);
 
     }
-
 
 
 }
